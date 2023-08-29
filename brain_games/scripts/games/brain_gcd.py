e630c5a1d227c13f88693ff3fdd32cc896dac0eb
Oleg <antoshinov@gmail.com>
2023-08-29 11:30:24 +1000

    'add-game'

---
+++ brain_games/scripts/games/brain_gcd.py
@@ -0,0 +1,14 @@
+from brain_games.engine import playing
+from random import randint
+import math
+
+
+def main():
+	condition = 'Find the greatest common divisor of given numbers.'
+	numbers = [[randint(1, 100), randint(1, 100)] for _ in range(3)]
+	results = [str(math.gcd(*numbers[i])) for i in range(3)]
+	playing(condition, numbers, results)
+
+
+if __name__ == '__main__':
+	main()
