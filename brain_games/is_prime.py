ade56d724a085a24465ab378d517467518a75957
Oleg <antoshinov@gmail.com>
2023-08-30 15:05:36 +1000

    'modified'

--- brain_games/is_prime.py
+++ brain_games/is_prime.py
@@ -1,8 +1,7 @@
 def is_prime(number):
     if number < 2:
         return False
-    else:
-        for i in range(2, number//2):
-            if number % i == 0:
-                return False
-        return True
+    for i in range(2, number//2):
+        if number % i == 0:
+            return False
+    return True
