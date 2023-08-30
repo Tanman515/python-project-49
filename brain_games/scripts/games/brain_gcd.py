b2566c42cba8083f9a0a6ff2d775e28db067a533
Oleg <antoshinov@gmail.com>
2023-08-30 15:22:48 +1000

    'update-game'

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -7,6 +7,7 @@ def main():
     condition = 'Find the greatest common divisor of given numbers.'
     numbers = [[randint(1, 100), randint(1, 100)] for _ in range(3)]
     results = [str(math.gcd(*numbers[i])) for i in range(3)]
+    numbers = [f'{str(first)} {str(second)}' for first, second in numbers] # noqa
     playing(condition, numbers, results)
 
 
