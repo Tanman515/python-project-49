888b1f936c63128df68a373ba093ff01da79e871
Oleg <antoshinov@gmail.com>
2023-09-12 14:29:04 +1000

    'update_engine'

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,22 +1,27 @@
 # General game architecture
 
+from brain_games.cli import welcome_user
+
 
 TRIES = 3
 FIRST_TRY = 0
 LAST_TRY = 2
 
 
-def playing(condition, expression, answer, attempt, name):
-    if attempt == FIRST_TRY:
-        print(condition)
-    print(f'Question: {expression}')
-    user_answer = input('Your answer: ')
-    if user_answer == answer:
-        print('Correct!')
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'") # noqa
-        print(f"Let's try again, {name}!")
-        return False
-    if attempt == LAST_TRY:
-        print(f'Congratulations, {name}!')
-    return True
+def run(game_func):
+    name = welcome_user()
+    for attempt in range(TRIES):
+        condition, expression, answer = game_func()
+        if attempt == FIRST_TRY:
+            print(condition)
+        print(f'Question: {expression}')
+        user_answer = input('Your answer: ')
+        if user_answer == answer:
+            print('Correct!')
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'") # noqa
+            print(f"Let's try again, {name}!")
+            break
+        if attempt == LAST_TRY:
+            print(f'Congratulations, {name}!')
+
